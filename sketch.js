62a7dcf3ab898640a7cf3291b4ae0d921abe040e
Eric LaMothe <32689445+lamothee@users.noreply.github.com>
2020-03-26 11:57:34 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,11 +3,12 @@
 // https://thecodingtrain.com/Courses/ml5-beginners-guide/7.1-posenet.html
 // https://youtu.be/OIo-DIOkNVg
 // https://editor.p5js.org/codingtrain/sketches/ULA97pJXR
-
+'use strict';
 let video;
 let poseNet;
 let pose;
 let skeleton;
+let osc;
 
 function setup() {
   createCanvas(640, 480);
@@ -15,10 +16,13 @@ function setup() {
   video.hide();
   poseNet = ml5.poseNet(video, modelLoaded);
   poseNet.on('pose', gotPoses);
+  osc = new p5.Oscillator();
+
+
 }
 
 function gotPoses(poses) {
-  //console.log(poses); 
+  //console.log(poses);
   if (poses.length > 0) {
     pose = poses[0].pose;
     skeleton = poses[0].skeleton;
@@ -37,30 +41,18 @@ function draw() {
     let eyeR = pose.rightEye;
     let eyeL = pose.leftEye;
     let d = dist(eyeR.x, eyeR.y, eyeL.x, eyeL.y);
-    fill(255, 0, 0);
-    ellipse(pose.nose.x, pose.nose.y, d);
     fill(0, 0, 255);
-    ellipse(pose.rightWrist.x, pose.rightWrist.y, 32);
-    ellipse(pose.leftWrist.x, pose.leftWrist.y, 32);
-    
-    for (let i = 0; i < pose.keypoints.length; i++) {
-      let x = pose.keypoints[i].position.x;
-      let y = pose.keypoints[i].position.y;
-      fill(0,255,0);
-      ellipse(x,y,16,16);
-    }
-    
-    for (let i = 0; i < skeleton.length; i++) {
-      let a = skeleton[i][0];
-      let b = skeleton[i][1];
-      strokeWeight(2);
-      stroke(255);
-      line(a.position.x, a.position.y,b.position.x,b.position.y);      
-    }
-    
-    
-  
-  
-  
+    osc.freq(d*20);
+    osc.amp(.2);
+    console.log(d);
+    tint(d*2, d/2, d/2);
+
   }
+}
+function mousePressed() {
+  osc.start();
+}
+
+function mouseReleased() {
+  osc.stop();
 }
